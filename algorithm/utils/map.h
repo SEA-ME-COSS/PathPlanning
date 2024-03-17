34f8778f9a476acfa05e3b889976380e52d37171
dongdongO <tjehdghks4356@gmail.com>
2024-03-17 21:30:57 +0100

    update bool type Map

--- algorithm/utils/map.h
+++ algorithm/utils/map.h
@@ -23,14 +23,13 @@ private:
     bool is_outside(std::vector<int> point);
 
     std::vector<std::vector<Point>> grid_map;
+    std::vector<std::vector<bool>> booleanMapData;
 };
 
 Map::Map(const std::string& map_path) {
     std::vector<std::vector<int>> map_data = load_map_data(map_path);
     this->rows = map_data[0].size();
     this->cols = map_data.size();
-
-    int extra = 1;
 
     std::cout << rows << " , " << cols << std::endl;
 
@@ -59,6 +58,17 @@ Map::Map(const std::string& map_path) {
             }
         }
     }
+
+
+    booleanMapData.resize(rows, std::vector<bool>(cols, false));
+
+    for (int y = 0; y < cols; ++y) {
+        for (int x = 0; x < rows; ++x) {
+            uint8_t map_value = map_data[y][x];
+            // Assuming 1 and 6 represent blocked areas
+            booleanMapData[x][y] = map_value == 1 || map_value == 6;
+        }
+    }
     std::cout << "Map Size: " << grid_map.size() << " x " << grid_map[0].size() << std::endl;    
 }
 
@@ -73,15 +83,25 @@ std::vector<int> Map::get_map_info() {
     return map_info;
 }
 
+// bool Map::is_blocked(std::vector<int> point) {
+//     if (is_outside(point)) {return true;}
+
+//     if (grid_map[point[0]][point[1]].blocked) {return true;}
+//     return false;
+// }
+
+// bool Map::is_outside(std::vector<int> point) {
+//     if (point[0]<0 || rows<=point[0]) {return true;}
+//     else if (point[1]<0 || cols<=point[1]) {return true;}
+//     else {return false;}
+// }
+
 bool Map::is_blocked(std::vector<int> point) {
-    if (is_outside(point)) {return true;}
+    // 지도 바깥에 있는지 확인
+    if (point[0] < 0 || point[0] >= rows || point[1] < 0 || point[1] >= cols) {
+        return true; // 지도 바깥에 있는 경우, 자동으로 차단된 것으로 처리
+    }
 
-    if (grid_map[point[0]][point[1]].blocked) {return true;}
-    return false;
-}
-
-bool Map::is_outside(std::vector<int> point) {
-    if (point[0]<0 || rows<=point[0]) {return true;}
-    else if (point[1]<0 || cols<=point[1]) {return true;}
-    else {return false;}
+    // 지도 내부의 지점이 차단되었는지 확인
+    return booleanMapData[point[0]][point[1]];
 }
