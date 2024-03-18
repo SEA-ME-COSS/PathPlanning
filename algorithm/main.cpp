260fd1e0a57d90c39429b83f033daddc3f2be300
dongdongO <tjehdghks4356@gmail.com>
2024-03-18 15:13:47 +0100

    #3 local test setup finished

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -15,36 +15,34 @@ namespace plt = matplotlibcpp;
 int main() {
     // Load Waypoints Info
     std::string nodes_file_path = "../globalmap/parsinginfo.txt";
-    std::string waypoints_file_path = "../globalmap/test-waypoints.txt";
+    std::string waypoints_file_path = "../globalmap/waypoints.txt";
     std::vector<std::vector<double>> waypoints = load_waypoints(nodes_file_path, waypoints_file_path);
 
-    // Make Map instruction
+    // Make Map Instruction
     std::string map_file_path = "../globalmap/flipped-track.txt";
     Map map = Map(map_file_path);
 
     // Path Planner (Using same map)
+    // auto planner = BFS(map);
     auto planner = A_Star(map);
-    // auto planner = A_Star(map);
     planner.plan_with_waypoints(waypoints);
     std::vector<std::vector<int>> route = planner.get_waypoints_path();
 
     // Only for drawing
     std::cout << "Draw Map" << std::endl;
     std::map<int, float> structure_color_map = {
-        {0, 1.0}, // solidline (실선)
-        {1, 0.5}, // stopline (정지선)
-        {2, 0.3}, // crosswalk (횡단보도)
-        {3, 0.4}, // dottedline (점선)
-        {4, 0.5}, // parkinglot (주차장)
-        {5, 0.6}, // roundabout (로터리)
-        {6, 0.7}, // startline (시작구역)
-        {7, 0.0}, // floor (차도)
-        {8, 0.8}, // busline (버스전용차선)
-        {9, 0.9} // sidewalk (인도 or 차 못가는 모든 곳)
+        {0, 1.0}, // Driveway
+        {1, 0.5}, // Solid Line
+        {2, 0.3}, // Parking Spot
+        {3, 0.4}, // Dotted Line
+        {4, 0.5}, // Stop Line
+        {5, 0.6}, // Crosswalk
+        {6, 0.7}, // Roundabout
+        {10, 0.9}, // Extra
     };
 
     // Draw map info
-    std::vector<std::vector<Point>> draw_grid_map = map.get_map();
+    std::vector<std::vector<int>> draw_grid_map = map.get_grid_map();
     std::vector<int> draw_map_info = map.get_map_info();
 
     int map_width = draw_map_info[0];
@@ -54,7 +52,7 @@ int main() {
 
     for(int x=0; x<map_width; ++x) {
         for(int y=0; y<map_height; ++y) {
-            int structure = draw_grid_map[x][y].structure;
+            int structure = draw_grid_map[x][y];
             int index = y * map_width + x;
             image_data[index] = structure_color_map[structure];
         }
